ff67be78b20aac2133925b5f86065bf839d4dc77
fedul04 <qwq1dwd04@gmail.com>
2025-12-18 16:40:45 +0300

    fix: restore CSS import in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 // ==================== FIX FOR RUSSIAN CHARACTERS IN BTOA ====================
 // btoa() doesn't support UTF-8 (Cyrillic) characters natively
